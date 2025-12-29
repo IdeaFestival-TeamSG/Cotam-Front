a63b0bd73e4ccf6ae7f66938ee61b4d44adfe60f
LeeSangHyeok0731 <s24066@gsm.hs.kr>
2025-12-30 08:53:14 +0900

    feat: add ProblemComponent for displaying problem details and navigation

--- src/components/ProblemComponent/index.tsx
+++ src/components/ProblemComponent/index.tsx
@@ -22,8 +22,8 @@ const ProblemComponent = ({
       </div>
       <div className="flex justify-between w-full items-center">
         <p className="text-[#717872]">{description}</p>
-        <div>
-          <p className={`text-[#717872] ${solved ? "text-[#32AB7B]" : "text-[#FF5555]"}`}>{solved ? "해결" : "미해결"}</p>
+        <div className="flex gap-2">
+          <p className={` ${solved ? "text-[#32AB7B]" : "text-[#FF3B55]"}`}>{solved ? "해결" : "미해결"}</p>
           <DifficultyLevel difficult={difficulty} /></div>
       </div>
     </div>
